3f6a8821e03129a251c6c7ff798a348866c6f6a5
Chris Kendall <onbrandapp@shaw.ca>
2026-07-16 07:43:17 -0700

    build: bump version to 20.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.expensetracker.mvkqxl"
     minSdk = 24
     targetSdk = 36
-    versionCode = 18
-    versionName = "18.0"
+    versionCode = 20
+    versionName = "20.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
