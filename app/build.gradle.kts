0eeca32553db1aeb354daa6c417a361ae8b3f306
Chris Kendall <feelgoodstudio2026@gmail.com>
2026-08-31 08:23:54 -0700

    build: bump version code to 50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.expensetracker.mvkqxl"
     minSdk = 24
     targetSdk = 36
-    versionCode = 49
-    versionName = "49.0"
+    versionCode = 50
+    versionName = "50.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
